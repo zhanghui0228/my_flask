8746403042cc9078cdcc48e6aa173cd2451cee4d
辉_少 <2318974133@qq.com>
2019-08-10 21:02:03 +0800

    更新了flask cookies的基本设置

---
+++ flask_cookies/READ.md
@@ -0,0 +1,1 @@
+flask cookies代码未完善，有问题，可以设置cookies
---
+++ flask_cookies/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+	if request.method == 'POST':
+		user = request.form['name']
+
+		resp = make_response(render_template('readcookie.html'))
+		resp.set_cookie('userID', user)
+		return resp
+
+@app.route('/getcookie')
+def getcookie():
+	name = request.cookie.get('userID')
+	print (name)
+	return '<h1>welcome, '+name+'</h1>'
+if __name__ == '__main__':
+	app.run('0.0.0.0', '8090', debug = True)
